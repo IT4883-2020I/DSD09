f31a6ba42b1573b17b2b0c51e2619c4aa288a0f2
dungnguyen <dungntt@bravebits.vn>
2020-11-18 08:35:36 +0700

    update gg map iframe

--- frontend/src/pages/Incidient/edit.jsx
+++ frontend/src/pages/Incidient/edit.jsx
@@ -132,8 +132,12 @@ const IncidentEdit = () => {
               <Form.Item label={'Tên sự cố'}><Input value={'Cây đổ vào chạm biến áp trên quốc lộ 32 km16'}></Input></Form.Item>
               <Form.Item label={'Mô tả'}><Input.TextArea rows={4} value={'Lúc 14h55’ ngày 15/4, tại khoảng cột 435/37, đường dây 471E58 của thôn Đắc Tà Vầng, xã Đắc Tôi, huyện Nam Giang, diều của người dân quanh khu vực thả lên bị đứt dây và vướng vào lưới điện, gây sự cố đường dây cấp điện một phần khu vực huyện.'}></Input.TextArea></Form.Item>
               <Form.Item label={'Ảnh'}><Images/></Form.Item>
-              <Form.Item label={'Vị trí'}><Input value={'km16 Quốc lộ 32, Huyện Hoài Đức, Hà Nội'}/></Form.Item>
-              <Form.Item><GGMap/></Form.Item>
+              <Form.Item label={'Vị trí'}><Input value={'Trạm điện cao thế Ngọc Liên'}/></Form.Item>
+              <Form.Item>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.5756715163598!2d106.1600463149287!3d20.96954899517287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313598a919dd3821%3A0x5ba5e686fd49047!2zVHLhuqFtIMSRaeG7h24gY2FvIHRo4bq_IE5n4buNYyBMacOqbg!5e0!3m2!1sen!2s!4v1605663218115!5m2!1sen!2s"
+                    width="100%" height="450" frameBorder="0" style={{border: 0}} allowFullScreen="" aria-hidden="false"
+                    tabIndex="0"></iframe></Form.Item>
 
             </Form>
           </Col>
@@ -145,8 +149,8 @@ const IncidentEdit = () => {
             <br/>
             <Form layout={'vertical'}>
               <Form.Item label={'Người tạo'}><Tag>Hệ thống</Tag></Form.Item>
-              <Form.Item label={'Người phân công'}><Select value={'1'}>{users.map(u => <Option value={u.value} label={u.label}/>)}</Select></Form.Item>
-              <Form.Item label={'Người được phân công'}><Select value={'2'}>{users.map(u => <Option value={u.value} label={u.label}/>)}</Select></Form.Item>
+              <Form.Item label={'Người phân công'}><Select value={'1'}>{users.map(u => <Option value={u.value}>{u.label}</Option>)}</Select></Form.Item>
+              <Form.Item label={'Người được phân công'}><Select value={'2'}>{users.map(u => <Option value={u.value}>{u.label}</Option>)}</Select></Form.Item>
               <Form.Item label={'Logged Time'}><Input value={'4'}/></Form.Item>
               <Form.Item label={'Due Date'}><DatePicker defaultValue={moment('2020/01/01', 'YYYY/MM/DD')} format={'YYYY/MM/DD'} /></Form.Item>
 
