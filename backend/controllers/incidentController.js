53244ee758f3b649f9f8592dc9232b8050a70c05
anhbui <vietanh@unstatic.co>
2020-11-22 18:09:39 +0700

    update filter

--- backend/controllers/incidentController.js
+++ backend/controllers/incidentController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Incident from "../models/incidentModel.js";
 import { validationResult } from "express-validator";
+import _ from "lodash";
 
 const getIncidents = asyncHandler(async (req, res) => {
   const errors = validationResult(req);
@@ -8,8 +9,8 @@ const getIncidents = asyncHandler(async (req, res) => {
     res.status(422).json({ errors: errors.array() });
     return;
   }
-  console.log(req.body.limit);
-  const incidents = await Incident.find({})
+  const findQuery = buildFindQuery(req.body);
+  const incidents = await Incident.find(findQuery)
     .limit(req.body.limit)
     .skip(req.body.offset)
     .sort({ updatedAt: -1 })
@@ -17,9 +18,15 @@ const getIncidents = asyncHandler(async (req, res) => {
     .populate("status")
     .populate("level")
     .exec();
+  const { status, level, assignee } = req.body;
   res.json(
-    incidents
-    // .filter((incident) => incident.type.type === req.user.type)
+    incidents.filter((incident) => {
+      if (level !== undefined && level !== incident.level.code) return false;
+      if (status !== undefined && status !== incident.status.code) return false;
+      if (assignee && assignee.length > 0 && _.difference(assignee, incident.assignee).length > 0)
+        return false;
+      return incident.type.type === req.user.type;
+    })
   );
 });
 
@@ -39,5 +46,19 @@ const getIncidentById = asyncHandler(async (req, res) => {
     res.json({ message: "Incident not found" });
   }
 });
+
+const buildFindQuery = (payload) => {
+  const findQuery = {};
+  if (payload.name) findQuery.name = { $regex: payload.name, $options: "i" };
+  if (payload.location) findQuery.location = { $regex: payload.location, $options: "i" };
+  if (payload.dueDateMin) {
+    findQuery.dueDate = { $gte: new Date(payload.dueDateMin) };
+  }
+  if (payload.dueDateMax) {
+    let dueDateMax = new Date(payload.dueDateMax);
+    findQuery.dueDate = { ...findQuery.dueDate, $lt: dueDateMax.setDate(dueDateMax.getDate() + 1) };
+  }
+  return findQuery;
+};
 
 export { getIncidents, getIncidentById };
--- backend/validator/incidents.js
+++ backend/validator/incidents.js
@@ -2,10 +2,20 @@ import { check } from "express-validator";
 
 const validateGetIncidentsList = () => {
   return [
-    check("page", "Page must be numeric").isNumeric().optional({ nullable: true }),
-    check("limit", "Limit must be numeric").isNumeric().optional({ nullable: true }),
+    check("offset", "Page must be numeric > 0").isInt({ min: 0 }).optional({ nullable: true }),
+    check("limit", "Limit must be numeric > 0").isInt({ min: 0 }).optional({ nullable: true }),
     check("level", "Level must be numeric").isNumeric().optional({ nullable: true }),
-    check("status", "Status must be numeric").isNumeric().optional({ nullable: true })
+    check("status", "Status must be numeric").isNumeric().optional({ nullable: true }),
+    check("dueDateMin", "Invalid Due date").isISO8601("yyyy-mm-dd").optional({ nullable: true }),
+    check("dueDateMax", "Invalid Due date").isISO8601("yyyy-mm-dd").optional({ nullable: true }),
+    check("assignee", "Assignee must be array of number")
+      .isArray()
+      .custom((a) => {
+        return a.every((e) => {
+          return Number.isInteger(e);
+        });
+      })
+      .optional({ nullable: true })
   ];
 };
 
